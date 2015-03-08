1dbb303b155a94c2751b15942d0300f28b568413
mlloewen <mlloewen@gmail.com>
2015-03-07 18:18:55 -0700

    correction of script location

--- wpinit.sh
+++ wpinit.sh
@@ -2,4 +2,3 @@
 cd /data/src/html/wordpress/wp-content/plugins
 if [ ! -f /data/src/html/wordpress/wp-content/plugins/revisr/readme.txt ]; then
 	git clone https://github.com/ExpandedFronts/Revisr.git
-if
